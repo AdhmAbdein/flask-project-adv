c34dda279b409cb9da81b7155213f71d4286ec5c
root <root@master.localdomain>
2024-12-29 23:48:01 +0100

    initial commit

--- project-code/config.py
+++ project-code/config.py
@@ -1,4 +1,7 @@
+
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://adham:1234@postgres-service:5432/devopsDB"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "postgresql://adham:1234@localhost:5432/devopsDB")
+
 
